e5104cf51fdb40dd2a01478dbbbf516f62892c80
JethroNatividad <natividad.jet@gmail.com>
2026-01-19 06:00:47 +0800

    feat: restructure Dashboard component and move to dedicated file

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Dashboard from "./pages/dashboard";
+import Dashboard from "./pages/dashboard/dashboard";
 import { BrowserRouter, Routes, Route } from "react-router";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
